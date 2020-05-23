89eeebbc4796c2bb3ee662fda6b58bb47ad339a2
Mikko Metso <mikko.metso@sok.fi>
2020-05-23 16:46:53 +0300

    started adding bcrypt to the mix

--- models/User.js
+++ models/User.js
@@ -1,9 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
 });
 
-module.exports= mongoose.model('User',UserSchema)
+UserSchema.pre("save", function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = bcrypt.hashSync(this.password, saltRounds);
+  next();
+});
+
+UserSchema.methods.comparePassword = function(plaintext, callback) {
+  return callback(null, bcrypt.compareSync(plaintext, this.password));
+};
+
+module.exports = mongoose.model("User", UserSchema);
